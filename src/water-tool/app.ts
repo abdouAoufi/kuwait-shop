fb30ebececcbb7ddd1fdda16d590a50979711721
Abderahmane Aoufi <aoufi.abdrahmane@lastingdynamics.com>
2024-07-06 23:02:26 +0100

    update pixel id

--- src/water-tool/app.ts
+++ src/water-tool/app.ts
@@ -182,7 +182,7 @@ const initFbPixel = () => {
       s = b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t, s);
     })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
-    fbq("init", "430584076588418");
+    fbq("init", "460539890063316");
     fbq("track", "PageView");
   `;
 
@@ -196,7 +196,7 @@ const initFbPixel = () => {
       height="1"
       width="1"
       style="display: none"
-      src="https://www.facebook.com/tr?id=430584076588418&ev=PageView&noscript=1"
+      src="https://www.facebook.com/tr?id=460539890063316&ev=PageView&noscript=1"
     />
   `;
 
