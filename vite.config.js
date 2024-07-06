452876acaa6e36c87284ac52ff3e2d095a150ba1
Abderahmane Aoufi <aoufi.abdrahmane@lastingdynamics.com>
2024-07-06 22:01:26 +0100

    rm

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        venom: resolve(__dirname, '/src/venom-bee/index.html'),
         "water-tool": resolve(__dirname, '/src/water-tool/index.html'),
         "thank-you": resolve(__dirname, '/src/thank-you/index.html')
       }
