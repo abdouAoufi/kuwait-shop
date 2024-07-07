7704e414728033bbc9a7f024e503774f14f93602
Abderahmane Aoufi <aoufi.abdrahmane@lastingdynamics.com>
2024-07-07 11:48:48 +0100

    add pixel condition

--- src/water-tool/app.ts
+++ src/water-tool/app.ts
@@ -43,11 +43,11 @@ document.addEventListener("DOMContentLoaded", async () => {
   // ! CHECK USER COUNTRY ==============================
   if (disallowedCountries.includes(locationData?.country)) return;
   handleAddGoogleAnalytics();
-  trackViewLead({
+  initFbPixel();      
+  ({
     country: locationData?.country,
     fbc: getQueryParam("fbclid"),
   }).then(console.log);
-  // initFbPixel();
   setTimeout(() => {
     if (window?.fbq)
       window?.fbq("track", "ViewContent", {
@@ -161,46 +161,45 @@ const handleAddGoogleAnalytics = () => {
   document.body.appendChild(noscript);
 };
 
-// const initFbPixel = () => {
-//   return;
-//   var fbScript = document.createElement("script");
-//   fbScript.innerHTML = `
-//     !(function (f, b, e, v, n, t, s) {
-//       if (f.fbq) return;
-//       n = f.fbq = function () {
-//         n.callMethod
-//           ? n.callMethod.apply(n, arguments)
-//           : n.queue.push(arguments);
-//       };
-//       if (!f._fbq) f._fbq = n;
-//       n.push = n;
-//       n.loaded = !0;
-//       n.version = "2.0";
-//       n.queue = [];
-//       t = b.createElement(e);
-//       t.async = !0;
-//       t.src = v;
-//       s = b.getElementsByTagName(e)[0];
-//       s.parentNode.insertBefore(t, s);
-//     })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
-//     fbq("init", "460539890063316");
-//     fbq("track", "PageView");
-//   `;
-//
-//   // Append script element to head
-//   document.head.appendChild(fbScript);
-//
-//   // Create noscript element
-//   var noScript = document.createElement("noscript");
-//   noScript.innerHTML = `
-//     <img
-//       height="1"
-//       width="1"
-//       style="display: none"
-//       src="https://www.facebook.com/tr?id=460539890063316&ev=PageView&noscript=1"
-//     />
-//   `;
-//
-//   // Append noscript element to body
-//   document.body.appendChild(noScript);
-// };
+const initFbPixel = () => {
+  var fbScript = document.createElement("script");
+  fbScript.innerHTML = `
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
+    fbq("init", "460539890063316");
+    fbq("track", "PageView");
+  `;
+
+  // Append script element to head
+  document.head.appendChild(fbScript);
+
+  // Create noscript element
+  var noScript = document.createElement("noscript");
+  noScript.innerHTML = `
+    <img
+      height="1"
+      width="1"
+      style="display: none"
+      src="https://www.facebook.com/tr?id=460539890063316&ev=PageView&noscript=1"
+    />
+  `;
+
+  // Append noscript element to body
+  document.body.appendChild(noScript);
+};
